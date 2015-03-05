775b7a41e8b7b2d30aa8085e762cb5f87b2ef4a6
Yichi Zhang <zhang-yi-chi@hotmail.com>
2015-03-06 03:09:46 +1100

    Updated demo project

---
+++ StringScoreDemo/StringScoreDemo/DemoViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  StringScoreDemo
+//
+//  Created by YICHI ZHANG on 21/02/2015.
+//  Copyright (c) 2015 YICHI ZHANG. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+	let stringsToTest = [
+		"Dingo in Wonderland",
+		"Melbourne Dingo",
+		"You Better Take Cover",
+		"Since 1997",
+		"Alice lives in Wonderland",
+		"Alice has a Dingo",
+		"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland",
+		"AHADALIWHHDNLIW",
+		"😄😅😆"
+	]
+	lazy var textView:UITextView = {
+		let tv = UITextView(frame: self.view.bounds)
+		tv.editable = false
+		return tv
+	}()
+	
+	func testDisplayStringFor(#string:String, word:String, fuzziness:Double? = nil) -> String {
+		
+		var t = "fuzziness = \(fuzziness).\n\(string) \nAGAINST \n \(word)\n"
+		
+		var z = 0.0
+		z = string.score(word: word, fuzziness: fuzziness)
+		
+		t = t + "\(z)\n\n"
+		
+		return t
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		var t = ""
+		let baseStrings = [
+			"😄😅😆 Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland, but he made a documentary called You Better Take Cover.",
+			"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland, but he made a documentary called You Better Take Cover.",
+			"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes 😄 😅 😆 does not live in Wonderland, but he made a documentary called You Better Take Cover.",
+			]
+		
+		let fuzzinessArray = [NSNull(), 0.5, 0.7, 1.0]
+		
+		for fuzziness in fuzzinessArray {
+			for baseString in baseStrings {
+				for string in stringsToTest {
+					if fuzziness.isKindOfClass(NSNull.self){
+						t = t + testDisplayStringFor(string: baseString, word: string)
+					} else if let f = fuzziness as? Double {
+						t = t + testDisplayStringFor(string: baseString, word: string, fuzziness: f)
+					}
+				}
+			}
+		}
+		
+		view.addSubview(textView)
+		textView.text = t
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
--- StringScoreDemo/StringScoreDemo/ViewController.swift
+++ StringScoreDemo/StringScoreDemo/ViewController.swift
@@ -8,71 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-	let stringsToTest = [
-		"Dingo in Wonderland",
-		"Melbourne Dingo",
-		"You Better Take Cover",
-		"Sience 1997",
-		"Alice lives in Wonderland",
-		"Alice has a Dingo",
-		"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland",
-		"AHADALIWHHDNLIW",
-		"😄😅😆"
-	]
-	lazy var textView:UITextView = {
-		let tv = UITextView(frame: self.view.bounds)
-		tv.editable = false
-		return tv
-	}()
+class ViewController: UIViewController, UITextViewDelegate {
 	
-	func testDisplayStringFor(#string:String, word:String, fuzziness:Double? = nil) -> String {
-		
-		var t = "fuzziness = \(fuzziness).\n\(string) \nAGAINST \n \(word)\n"
-		
-		var z = 0.0
-		z = string.score(word: word, fuzziness: fuzziness)
-		
-		t = t + "\(z)\n\n"
-		
-		return t
-	}
+	@IBOutlet weak var sourceTextView: UITextView!
+	@IBOutlet weak var searchTextField: UITextField!
+	@IBOutlet weak var fuzzinessSlider: UISlider!
+	@IBOutlet weak var resultLabel: UILabel!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
-		var t = ""
-		let baseStrings = [
-			"😄😅😆 Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland, but he made a documentary called You Better Take Cover.",
-			"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes does not live in Wonderland, but he made a documentary called You Better Take Cover.",
-			"Alice has a Dingo. Alice lives in Wonderland. Harry Hayes 😄 😅 😆 does not live in Wonderland, but he made a documentary called You Better Take Cover.",
-			]
+		searchTextField.text = "Alice 😄 😅 "
+	
+		sourceTextView.text = "Alice has a Dingo. 😄 😅 😆 Alice lives in Wonderland."
+		sourceTextView.delegate = self
 		
-		let fuzzinessArray = [NSNull(), 0.5, 0.7, 1.0]
+		searchTextField.addTarget(self, action: "controlValueChanged:", forControlEvents: UIControlEvents.EditingChanged)
+		fuzzinessSlider.addTarget(self, action: "controlValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
 		
-		for fuzziness in fuzzinessArray {
-			for baseString in baseStrings {
-				for string in stringsToTest {
-					if fuzziness.isKindOfClass(NSNull.self){
-						t = t + testDisplayStringFor(string: baseString, word: string)
-					} else if let f = fuzziness as? Double {
-						t = t + testDisplayStringFor(string: baseString, word: string, fuzziness: f)
-					}
-				}
-			}
-		}
+		self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "backgroundTouched:"))
 		
-		view.addSubview(textView)
-		textView.text = t
+		updateStringScore()
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
+	// MARK: Slider value changed
+	func controlValueChanged(sender:AnyObject) {
+		updateStringScore()
+	}
 
-
+	// MARK: Background touched
+	func backgroundTouched(sender:AnyObject) {
+		sourceTextView.resignFirstResponder()
+		searchTextField.resignFirstResponder()
+	}
+	
+	// MARK: Update
+	func updateStringScore() {
+		let sourceText = sourceTextView.text
+		let searchText = searchTextField.text
+		
+		let score = sourceText.score(word: searchText, fuzziness: Double(fuzzinessSlider.value))
+		resultLabel.text = NSString(format: "%.3f", score)
+	}
+	
+	// MARK: UITextViewDelegate
+	func textViewDidChange(textView: UITextView) {
+		updateStringScore()
+	}
 }
 
