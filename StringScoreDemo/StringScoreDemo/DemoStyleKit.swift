9c93b332a13bedb00b39cc4217ce3871a51961d7
Yichi Zhang <zhang-yi-chi@hotmail.com>
2016-04-29 20:09:45 +1000

    Update code formatting; use 4 spaces instead of tab for indentation

--- StringScoreDemo/StringScoreDemo/DemoStyleKit.swift
+++ StringScoreDemo/StringScoreDemo/DemoStyleKit.swift
@@ -14,56 +14,61 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 import UIKit
 
-public class DemoStyleKit : NSObject {
-	
-	class var mainFont:UIFont {
-		return UIFont(name: "HelveticaNeue-LightItalic", size: 20)!
-	}
-	
-	//// Cache
-	
-	private struct Cache {
-		static var imageDict : [String:UIImage] = Dictionary()
-		//        static var oneTargets: [AnyObject]?
-	}
-	
-	//// Drawing Methods
-	
-	public class func draw(string string:String) {
-		//// General Declarations
-		let context = UIGraphicsGetCurrentContext()
-		
-		//// Text Drawing
-		let textRect = CGRectMake(0, 0, 25, 25)
-		let textTextContent = NSString(string: string)
-		let textStyle = NSMutableParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
-		textStyle.alignment = NSTextAlignment.Center
-		
-		let textFontAttributes = [NSFontAttributeName: DemoStyleKit.mainFont, NSForegroundColorAttributeName: UIColor.blackColor(), NSParagraphStyleAttributeName: textStyle]
-		
-		let textTextHeight: CGFloat = textTextContent.boundingRectWithSize(CGSizeMake(textRect.width, CGFloat.infinity), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: textFontAttributes, context: nil).size.height
-		CGContextSaveGState(context)
-		CGContextClipToRect(context, textRect);
-		textTextContent.drawInRect(CGRectMake(textRect.minX, textRect.minY + (textRect.height - textTextHeight) / 2, textRect.width, textTextHeight), withAttributes: textFontAttributes)
-		CGContextRestoreGState(context)
-	}
-	
-	//// Generated Images
-	
-	public class func imageOf(string string:String) -> UIImage {
-		if let image = Cache.imageDict[string] {
-			return image
-		}
-		
-		UIGraphicsBeginImageContextWithOptions(CGSizeMake(25, 25), false, 0)
-		DemoStyleKit.draw(string: string)
-		
-		let image = UIGraphicsGetImageFromCurrentImageContext()!
-		UIGraphicsEndImageContext()
-		
-		Cache.imageDict[string] = image
-		
-		return image
-	}
-	
+public class DemoStyleKit: NSObject
+{
+
+    class var mainFont: UIFont
+    {
+        return UIFont(name: "HelveticaNeue-LightItalic", size: 20)!
+    }
+
+    //// Cache
+
+    private struct Cache
+    {
+        static var imageDict: [String:UIImage] = Dictionary()
+        //        static var oneTargets: [AnyObject]?
+    }
+
+    //// Drawing Methods
+
+    public class func draw(string string: String)
+    {
+        //// General Declarations
+        let context = UIGraphicsGetCurrentContext()
+
+        //// Text Drawing
+        let textRect = CGRectMake(0, 0, 25, 25)
+        let textTextContent = NSString(string: string)
+        let textStyle = NSMutableParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+        textStyle.alignment = NSTextAlignment.Center
+
+        let textFontAttributes = [NSFontAttributeName: DemoStyleKit.mainFont, NSForegroundColorAttributeName: UIColor.blackColor(), NSParagraphStyleAttributeName: textStyle]
+
+        let textTextHeight: CGFloat = textTextContent.boundingRectWithSize(CGSizeMake(textRect.width, CGFloat.infinity), options: NSStringDrawingOptions.UsesLineFragmentOrigin, attributes: textFontAttributes, context: nil).size.height
+        CGContextSaveGState(context)
+        CGContextClipToRect(context, textRect);
+        textTextContent.drawInRect(CGRectMake(textRect.minX, textRect.minY + (textRect.height - textTextHeight) / 2, textRect.width, textTextHeight), withAttributes: textFontAttributes)
+        CGContextRestoreGState(context)
+    }
+
+    //// Generated Images
+
+    public class func imageOf(string string: String) -> UIImage
+    {
+        if let image = Cache.imageDict[string] {
+            return image
+        }
+
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(25, 25), false, 0)
+        DemoStyleKit.draw(string: string)
+
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        Cache.imageDict[string] = image
+
+        return image
+    }
+
 }
